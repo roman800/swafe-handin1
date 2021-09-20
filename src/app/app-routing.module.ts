48db820fad8cd97b456c08aa0f94ed92f15a9363
Frosendal <frederik.rosendal@hotmail.com>
2021-09-20 14:43:07 +0200

    Added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreditCardAddComponent } from './credit-card/credit-card-add/credit-card-add.component';
+import { HomeComponent } from './home/home.component';
+import { TransactionListComponent } from './transaction/transaction-list/transaction-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'add-card',
+    component: CreditCardAddComponent
+  },
+  {
+    path: 'transaction-screen', 
+    component: TransactionListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
