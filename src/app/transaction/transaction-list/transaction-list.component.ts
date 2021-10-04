70491ecf310c2b910a7864a91dd6b5bcd9113f85
Frosendal <frederik.rosendal@hotmail.com>
2021-10-04 14:44:09 +0200

    Added transaction filter

--- src/app/transaction/transaction-list/transaction-list.component.ts
+++ src/app/transaction/transaction-list/transaction-list.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSelectChange } from '@angular/material/select';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { Observable } from 'rxjs';
-import { filter, map, mergeAll } from 'rxjs/operators';
+import { merge, Observable } from 'rxjs';
+import { filter, map, mergeAll, toArray } from 'rxjs/operators';
 import { CreditCard } from 'src/app/credit-card/credit-card-model';
 import { CreditCardService } from 'src/app/credit-card/credit-card-service';
 import { Transaction } from '../transaction.model';
@@ -13,8 +14,8 @@ import { TransactionService } from '../transaction.service';
   styleUrls: ['./transaction-list.component.scss'],
 })
 export class TransactionListComponent {
-  transactions$?: Observable<Transaction[]> = this.transactionService.get();
-  creditCards$?: Observable<CreditCard[]> = this.creditCardService.get();
+  transactions$: Observable<Transaction[]> = this.transactionService.get();
+  creditCards$: Observable<CreditCard[]> = this.creditCardService.get();
 
   constructor(
     private transactionService: TransactionService,
@@ -35,5 +36,15 @@ export class TransactionListComponent {
     });
   }
 
-  filterChange() {}
+  filterChange(selectedOption: MatSelectChange) {
+    this.transactions$ = this.transactionService.get().pipe(
+      mergeAll(),
+      filter(
+        (transaction) =>
+          transaction.credit_card.card_number ===
+          (selectedOption.value as number)
+      ),
+      toArray()
+    );
+  }
 }
